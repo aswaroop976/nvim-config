cd18360e5fd9ca62ea977f2060dd0e18decfdd66
aswaroop976 <arpan.swaroop@gmail.com>
2024-01-22 13:50:31 -0600

    comments

--- init.lua
+++ init.lua
@@ -1,17 +1,5 @@
 vim.api.nvim_set_option("clipboard","unnamed")
---let g:clipboard = {
---	\   'name': 'WslClipboard',
---	\   'copy': {
---	\      '+': 'clip.exe',
---	\      '*': 'clip.exe',
---	\    },
---	\   'paste': {
---	\      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---	\      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---	\   },
---	\   'cache_enabled': 0,
---	\ }
 require("arpan")
 require("arpan.set")
 require("arpan.remap")
-require("arpan.clipboard")
+require("arpan.clipboard") -- to allow copy pasting to and from clipboard
