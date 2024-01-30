30e22fe9b281727992fcbee1502db1cbd8fc8cca
aswaroop976 <arpan.swaroop@gmail.com>
2024-01-30 13:25:08 -0600

    added copy to clipboard key remap

--- lua/arpan/remap.lua
+++ lua/arpan/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
-
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 --vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
