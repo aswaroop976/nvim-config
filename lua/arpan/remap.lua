4fd80ff957b80427d28d861e2a45d4678225e6a4
aswaroop976 <arpan.swaroop@gmail.com>
2024-03-07 18:44:04 -0600

    remapped escape to kj and jk

--- lua/arpan/remap.lua
+++ lua/arpan/remap.lua
@@ -3,4 +3,6 @@ vim.g.maplocalleader = ","
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "kj", "<Esc>")
 --vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
