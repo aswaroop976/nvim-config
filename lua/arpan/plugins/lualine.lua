a7c625bec78b6cc8b2ec4ccdddbac8651704e179
aswaroop976 <arpan.swaroop@gmail.com>
2024-03-28 13:48:15 -0500

    added some new key remaps to make jumping around windows easier

--- lua/arpan/plugins/lualine.lua
+++ lua/arpan/plugins/lualine.lua
@@ -1,9 +1,13 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = { theme = "nightfly" },
-		})
-	end,
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            options = { theme = "nightfly" },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {},
+        })
+    end,
 }
--- lua/arpan/remap.lua
+++ lua/arpan/remap.lua
@@ -7,4 +7,11 @@ vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("i", "kj", "<Esc>")
 vim.keymap.set("v", "kj", "<Esc>")
 vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("n", "<leader>v", vim.cmd.vs)
+vim.keymap.set("n", "<leader>l", "<C-w>l")
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+
+
 --vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
